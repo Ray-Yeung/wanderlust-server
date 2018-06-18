85bf80992f94eff3e678508c2b1d4c9860dea61a
ssheaks <sarah.sheaks@gmail.com>
2018-06-18 11:12:47 -0700

    add places.json

--- models/place.js
+++ models/place.js
@@ -3,20 +3,41 @@
 const mongoose = require('mongoose');
 
 //schema to represent a place
+//need to add these: address: 
+// {type: String},
 const placeSchema = mongoose.Schema({
   name: 
     {type: String, 
       required: true},
-  address: 
-      {type: String},
   location: {
-    type: {type: String},
-    coordinates: []
+    lat: String,
+    lng: String
   },
   photos: [],
   place_id:
     {type: String},
+  types: [String],
   price_level: {type: Number},
-  rating: {type: Number},
-  types: []
+  rating: {type: Number, min: 0, max: 5},
+  phone_number: {type: String},
+  website: {type: String},
+  userId: 
+    {type: String,
+      required: true},
+  trip_id: 
+    {type: String}
 });
+
+//more efficient mongoose search
+placeSchema.index({ place_id: 1}, {unique: true});
+
+//return _id as id
+placeSchema.set('toObject', {
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
+module.exports = mongoose.model('Place', placeSchema);
---
+++ routes/places.js
@@ -0,0 +1,10 @@
+'use strict';
+const express = require('express');
+
+const Place = require('../models/place');
+const router = express.Router();
+const passport = require('passport');
+
+//if we build out a 'my trips' folder will need to validate the trip id
+
+
